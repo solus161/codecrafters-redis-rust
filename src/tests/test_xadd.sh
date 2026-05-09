e175d28a33d35fad84c0a3ef527ff0399540e6a2
solus161 <solus161@gmail.com>
2026-05-09 18:40:22 +0700

    codecrafters submit [skip ci]

--- src/tests/test_xadd.sh
+++ src/tests/test_xadd.sh
@@ -2,5 +2,9 @@
 {
   printf "*5\r\n\$4\r\nXADD\r\n\$9\r\npineapple\r\n\$3\r\n1-2\r\n\$6\r\nbanana\r\n\$9\r\nraspberry\r\n"
   printf "*5\r\n\$4\r\nXADD\r\n\$9\r\npineapple\r\n\$3\r\n1-2\r\n\$6\r\nbanana\r\n\$9\r\nraspberry\r\n"
+  
+  printf "*5\r\n\$4\r\nXADD\r\n\$3\r\nkey\r\n\$3\r\n1-*\r\n\$6\r\nbanana\r\n\$9\r\nraspberry\r\n"
+  printf "*5\r\n\$4\r\nXADD\r\n\$3\r\nkey\r\n\$3\r\n1-*\r\n\$6\r\nbanana\r\n\$9\r\nraspberry\r\n"
+  printf "*5\r\n\$4\r\nXADD\r\n\$3\r\nkey\r\n\$1\r\n*\r\n\$6\r\nbanana\r\n\$9\r\nraspberry\r\n"
 
 } | nc localhost 6379
